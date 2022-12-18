5a463d655192fe9df47dc4b6f586eed417de79ab
baozoudashijian <1787009521@qq.com>
2022-12-18 18:03:25 +0800

    feat: 实现get、post请求

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new name: 'ronger'
+    if user.save
+      render json: user
+    else
+      render json: user.errors
+    end
+  end
+
+  def show
+    user = User.find_by_id params[:id]
+    if user 
+      render json: user
+    else
+      head 404
+    end
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/create'
+  get 'user/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
 end
